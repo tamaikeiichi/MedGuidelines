68c6c30c3da0b21591e686ebafef5ee652b99696
tamai <kpiyota@yahoo.co.jp>
2024-12-16 07:14:23 +0900

    202412160714_home

--- app/src/main/java/com/example/medguidelines/ui/screen/listItem.kt
+++ app/src/main/java/com/example/medguidelines/ui/screen/listItem.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.medguidelines.R
-import com.example.medguidelines.data.IndexNames
 import java.security.MessageDigest
 
 @Composable
