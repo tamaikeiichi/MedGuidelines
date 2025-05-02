9801c99975357e165edb04b7f3ed898bfed451ec
tamaikeiichi <kpiyota@yahoo.co.jp>
2025-05-02 10:53:29 +0900

    202505021053_office

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.keiichi.medguidelines"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.01"
+        versionCode = 3
+        versionName = "1.02"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
