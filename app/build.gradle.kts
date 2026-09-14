d31c89a2b01e3dacf2e4f79cf0e8c9ad896339ff
tamaikeiichi <kpiyota@yahoo.co.jp>
2026-09-14 15:50:56 +0900

    copy function

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.keiichi.medguidelines"
         minSdk = 29
         targetSdk = 36
-        versionCode = 75
-        versionName = "1.70"
+        versionCode = 76
+        versionName = "1.71"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
